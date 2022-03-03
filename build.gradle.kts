6524206ab024fa289ba889acd048fdc761ed4034
Vojta <januvojta@gmail.com>
2022-03-03 20:47:25 +0100

    init template

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    kotlin("multiplatform") version "1.6.10"
+    id("org.jetbrains.compose") version "1.1.0"
+}
+
+group = "me.farmen"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+kotlin {
+    /* Targets configuration omitted. 
+    *  To find out how to configure the targets, please follow the link:
+    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
+
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.web.core)
+                implementation(compose.runtime)
+            }
+        }
+        val jsTest by getting{
+
+        }
+    }
+}
